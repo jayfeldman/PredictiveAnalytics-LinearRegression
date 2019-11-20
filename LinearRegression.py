afed785a0817c64b3a7de0937d38c80bfea09fe0
Jay Feldman <52713709+jayfeldman@users.noreply.github.com>
2019-11-19 23:04:35 -0800

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -9,25 +9,16 @@ from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
 
-lights = []
-yields = []
-row = []
+datafile = "LightsYield.csv"
+data = pd.read_csv(datafile)
 
-f = open('LightsYield.csv', 'r')
-f1 = f.readlines()
+slope, intercept, r_value, p_value, std_err = stats.linregress(data["Lights"], data["Yield"])    
 
-for x in f1:
-    row = np.array(x.split(',') )
-    lights.append(float(row[0].strip('\n')))
-    yields.append(float(row[1].strip('\n')))
-    
-slope, intercept, r_value, p_value, std_err = stats.linregress(lights,yields)    
-
-plt.scatter(lights, yields, alpha=0.5)
+plt.scatter(data["Lights"], data["Yield"], alpha=0.5)
 plt.title('Yield by Number of Lights')
 plt.xlabel('Number of Lights')
 plt.ylabel('Yield (oz)')
-plt.plot(lights, slope*np.array(lights) + intercept, 'r')
+plt.plot(data["Lights"], slope*np.array(data["Lights"]) + intercept, 'r')
 plt.show()
  
 print("Best Fit: Slope = {:.2f} Intercept = {:.2f}. R Value = {:.2f}".format(slope, intercept, r_value**2))
